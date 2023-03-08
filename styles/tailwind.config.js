6b2612746b9e772132d8ce6ba3f5aa8ce84e6c8e
Conor McAfee <hello@conormcafee.co.uk>
2023-03-08 23:02:27 +0000

    Progress

---
+++ styles/tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: ['_site/**/*.html'],
+  safelist: [],
+  theme: {
+    fontFamily: {
+      'title': ['Zeyada', 'cursive'],
+      'copy': ['Playfair Display', 'serif'],
+    },
+    extend: {
+      colors: {
+        'accent': '#ddb53c'
+      }
+    }
+  },
+  plugins: [],
+}
